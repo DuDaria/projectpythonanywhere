22f9a6ffb6f3b85ade17d18835b53128e621e84e
DuDaria <60156592+DuDaria@users.noreply.github.com>
2021-06-16 02:25:13 +0400

    update

--- projectpythonanywhere/blogproject/advito/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class AdvitoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'advito'
-
-    def ready(self):
-        from . import signals
